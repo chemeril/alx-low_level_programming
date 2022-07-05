61bd2e599d4b18d5c5557afd40e57596fa42ca19
chemeril <bug101xtreme2@gmail.com>
2022-07-05 13:59:54 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,32 @@
-#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+
+*main printing putchar                                                                                                                  
+
+*return - 0                                                                                                                             
+
+*/                                                                                                                                      
 
                                                                                                                                         
 
 int main(){                                                                                                                             
 
-printf("_putchar\n");
+_putchar('_');                                                                                                                          
+
+_putchar('p');                                                                                                                          
+
+_putchar('u');                                                                                                                          
+
+_putchar('t');                                                                                                                          
+
+_putchar('c');                                                                                                                          
+
+_putchar('h');                                                                                                                          
+
+_putchar('a');                                                                                                                          
+
+_putchar('r');                                                                                                                          
+
+_putchar(\n'); 
+
 return 0;
 }
