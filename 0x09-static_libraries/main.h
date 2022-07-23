f6010b1ec10923dd6eb51fe59f7663c16f8987ba
chemeril <bug101xtreme2@gmail.com>
2022-07-23 07:42:52 -0700

    first task

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifendef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -36,4 +39,6 @@ unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
 
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle);
+
+#endif
